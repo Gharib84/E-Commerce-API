4ce7a17443d2c31db724f3ffb8eac3e9aba9ca72
Gharib84e <ITprofessional63@gmail.com>
2025-03-10 22:33:45 +0100

    product service modified

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,9 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>  
+  ){}
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
