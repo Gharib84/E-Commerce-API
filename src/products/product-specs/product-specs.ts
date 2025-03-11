fc558878be49a4f574d37df3c8461ddd41ba354b
Gharib84e <ITprofessional63@gmail.com>
2025-03-11 16:37:20 +0100

    specfic product list created

--- src/products/product-specs/product-specs.ts
+++ src/products/product-specs/product-specs.ts
@@ -1,1 +1,20 @@
-export class ProductSpecs {}
+import { ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+
+@ValidatorConstraint({
+    name: 'isProductSpecs',
+    async: false
+})
+export class ProductSpecs implements ValidatorConstraintInterface {
+    accepetedSpecs = ['ram', 'processor', 'ssd', 'hdd', 'brand', 'model',
+        'color', 'weight', 'dimensions', 'material',
+        'capacity', 'power', 'voltage', 'warranty',
+        'condition', 'chip', 'year', 'other_features'];
+        
+    validate(value: any) {
+        return value && Object.keys(value).length > 0;
+    }
+
+    defaultMessage() {
+        return 'Product specs must not be empty';
+    }
+}
