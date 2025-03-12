a749278ceb2ce7683f53f169e631cc208e459628
Gharib84e <ITprofessional63@gmail.com>
2025-03-12 22:21:53 +0100

    order entity created and updated

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,4 +1,36 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
+import { OrderStatus } from '../enums/order-status';
 @Entity('orders')
-export class Order {}
+export class Order {
+    @PrimaryGeneratedColumn({
+        name: 'id',
+    })
+    id: number;
+
+    @ApiProperty({
+        description: 'Order QUantity',
+        type: 'number',
+    })
+    @Column({
+        name: 'quantity',
+        type: 'int',
+    })
+    quantity: number;
+
+    @ApiProperty({
+        description: 'Order status',
+    })
+    @Column({
+        name: 'status',
+        default:OrderStatus.PENDING
+    })
+    status: OrderStatus;
+
+    @Column('decimal', {
+        precision: 5,
+        scale: 2,
+        default: 0
+    })
+    totalPrice: number;
+}
