41012ecfa9f45f76bb7ce4b49b6bc8f91e01da0e
Gharib84e <ITprofessional63@gmail.com>
2025-03-12 22:00:34 +0100

    orderstatus enum generated

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,4 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+@Entity('orders')
 export class Order {}
---
+++ src/orders/enums/order-status.ts
@@ -0,0 +1,6 @@
+export enum OrderStatus {
+    PENDING = 'PANDING',
+    APPROVED = 'APPROVED',
+    DECLINED = 'DECLINED',
+    CANCELLED = 'CANCELLED',
+}
