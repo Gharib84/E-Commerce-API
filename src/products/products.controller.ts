d38204c8d528bf7209cd0957a01367f4d3fe3130
Gharib84e <ITprofessional63@gmail.com>
2025-03-12 21:34:01 +0100

    get products in paginated

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -45,6 +45,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  /**
+   * Updates an existing product in the database.
+   * @param id the id of the product to update
+   * @param updateProductDto the data to update in the product
+   * @returns a message indicating the update status
+   */
+
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
