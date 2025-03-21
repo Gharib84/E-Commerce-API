aded0075779294fbe74457ffc2bf17bb87240e91
Gharib84e <ITprofessional63@gmail.com>
2025-03-21 20:30:12 +0100

    order service modified - create new order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,11 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from './entities/order.entity';
+import { Product } from 'src/products/entities/product.entity';
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ){}
+  async create(createOrderDto: CreateOrderDto):Promise<Order | null> {
+    try {
+      const {productId,quantity,totalPrice}=createOrderDto;
+      const product = await this.productRepository.findOneBy({id:productId});
+
+      if(!product){
+        throw new Error('Product not found');
+      }
+
+      const order = await this.orderRepository.create({
+        product,
+        quantity,
+        totalPrice
+      });
+
+      await this.orderRepository.save(order);
+    
+
+    } catch (error) {
+      throw new Error(error.message);
+    }
+
+    return null;
   }
 
   findAll() {
