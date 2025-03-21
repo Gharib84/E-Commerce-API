cc852185f945ace8eb40ad56aca258b95c98567c
Gharib84e <ITprofessional63@gmail.com>
2025-03-21 19:48:40 +0100

    orders data transfer object created and updated

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,46 @@
-export class CreateOrderDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDecimal,IsPositive,IsString,IsUUID } from "class-validator";
+
+export class CreateOrderDto {
+    @ApiProperty({
+        description: 'Order ID',
+        type: 'string',
+    })
+    @IsString({
+        message:"Order ID should be a string"
+    })
+    @IsUUID('all',{
+        message:"Order ID should be a valid UUID"})
+    orderId:string
+
+    @IsString({
+        message:"prodcut id should be a string"
+    })
+    @ApiProperty({
+        description: 'Product ID',
+        type: 'string',
+    })
+    @IsUUID('all',{
+        message:"Product ID should be a valid UUID"
+    })
+    productId:string;
+
+    @IsPositive({
+        message:"Quantity should be a positive number"
+    })
+    @ApiProperty({
+        description: 'Order Quantity',
+        type: 'number',
+    })
+    quantity:number
+
+    @IsDecimal(
+        { decimal_digits: '2' },
+        { message: 'totalPrice must be a decimal number' },
+      )
+    @ApiProperty({
+        description: 'Product price',
+        type: 'number'
+    })
+    totalPrice:number
+}
