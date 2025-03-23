07a77e6a705d752c796f1edc481f47f188e301e6
Gharib84e <ITprofessional63@gmail.com>
2025-03-23 22:27:49 +0100

    common module modified & updated

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,9 +4,11 @@ import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Product } from 'src/products/entities/product.entity';
+import { PaginationService } from 'src/common/pagination.service';
+import { CommonModule } from 'src/common/common.module';
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
-  imports:[TypeOrmModule.forFeature([Order,Product])]
+  providers: [OrdersService,PaginationService],
+  imports:[TypeOrmModule.forFeature([Order,Product]),CommonModule]
 })
 export class OrdersModule {}
