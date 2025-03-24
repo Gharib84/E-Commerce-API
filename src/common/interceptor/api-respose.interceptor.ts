174dc7cb2ef73a2190464cd6ccdc9b2a9cee34ff
Gharib84e <ITprofessional63@gmail.com>
2025-03-24 22:46:51 +0100

    inteceptor for an entire api

---
+++ src/common/interceptor/api-respose.interceptor.ts
@@ -0,0 +1,28 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+import { ApiResponse } from '../types/api-response';
+@Injectable()
+export class ApiResposeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    handler: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return handler.handle().pipe(
+      map((data): ApiResponse => {
+        const message =
+          data && data.message ? data.message : 'Request successful';
+        if (data?.message) delete data.message;
+        const data_ =
+          data instanceof Error ? null : data?.data ? data.data : data;
+        const success = !(data instanceof Error) && data_ !== null;
+
+        return {
+          success,
+          data: data_,
+          error: data instanceof Error ? data : null,
+          message,
+        };
+      }),
+    );
+  }
+}
