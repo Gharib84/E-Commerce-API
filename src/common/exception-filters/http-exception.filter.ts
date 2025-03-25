81da3fd66c83742c05f2023937624f7b440e05e9
Gharib84e <ITprofessional63@gmail.com>
2025-03-25 15:26:07 +0100

    custom exception generated

--- src/common/exception-filters/http-exception.filter.ts
+++ src/common/exception-filters/http-exception.filter.ts
@@ -1,7 +1,18 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiResponse } from '../types/api-response';
-@Catch()
-export class HttpExceptionFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const errorMessage = exception.getResponse()['message'] ? exception.getResponse()['message'] : exception.message || 'Unknown error';
+   
+
+
+
+    //response.status(status).json(apiResponse);
+  }
 }
