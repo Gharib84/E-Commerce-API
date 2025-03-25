5e5d2cc356c4dc9a2e026ef19321a6c71aeb9a57
Gharib84e <ITprofessional63@gmail.com>
2025-03-25 15:30:39 +0100

    custom exception generated

--- src/common/exception-filters/http-exception.filter.ts
+++ src/common/exception-filters/http-exception.filter.ts
@@ -9,10 +9,16 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
     const errorMessage = exception.getResponse()['message'] ? exception.getResponse()['message'] : exception.message || 'Unknown error';
-   
+    const error:any = exception.getResponse()|| exception.getResponse()['error'];
+    delete error['message'];
 
+    const apiResponse: ApiResponse = {
+      success: false,
+      data: null,
+      error: error,
+      message: errorMessage,
+    };
 
-
-    //response.status(status).json(apiResponse);
+    response.status(status).json(apiResponse);
   }
 }
