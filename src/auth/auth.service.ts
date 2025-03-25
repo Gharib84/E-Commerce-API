e985e9779cfff0a3a6ea074f953cdcd552d12cad
Gharib84e <ITprofessional63@gmail.com>
2025-03-25 21:30:00 +0100

    auth service modified

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/entities/user.entity';
+import * as bcrypt from 'bcrypt-ts';
 
 @Injectable()
 export class AuthService {}
