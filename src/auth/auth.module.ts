7d64db7289774047a1e56329d94a88179483fecf
Gharib84e <ITprofessional63@gmail.com>
2025-03-25 21:09:18 +0100

    autentication controller created

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '24h' },
+        global: true
+      }),
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController]
 })
 export class AuthModule {}
