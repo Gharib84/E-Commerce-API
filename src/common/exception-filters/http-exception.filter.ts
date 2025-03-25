501a3ac802836138d4a31213e28f00163bf8c032
Gharib84e <ITprofessional63@gmail.com>
2025-03-25 15:34:36 +0100

    custom exception for an entire app had been finished

--- src/common/exception-filters/http-exception.filter.ts
+++ src/common/exception-filters/http-exception.filter.ts
@@ -4,6 +4,13 @@ import { ApiResponse } from '../types/api-response';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  /**
+   * This is the catch method of the HttpExceptionFilter.
+   * It will be called whenever an HttpException is thrown.
+   * It will format the error response into our standard ApiResponse format.
+   * @param exception The HttpException that has been thrown.
+   * @param host The ArgumentsHost object that provides the request context.
+   */
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
