487d509d7f920f34b9198d2906125d1e41efd1dd
Gharib84e <ITprofessional63@gmail.com>
2025-03-25 15:08:16 +0100

    custom exception generated

--- src/common/exception-filters/http-exception.filter.ts
+++ src/common/exception-filters/http-exception.filter.ts
@@ -1,5 +1,6 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { Response } from 'express';
+import { ApiResponse } from '../types/api-response';
 @Catch()
 export class HttpExceptionFilter<T> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {}
