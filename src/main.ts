93ac627b63e2803ef08c158a9b96b045d30d5a15
Gharib84e <ITprofessional63@gmail.com>
2025-03-25 15:40:44 +0100

    custom exception integrated with an entire api

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ApiResposeInterceptor } from './common/interceptor/api-respose.interceptor';
+import { HttpExceptionFilter } from './common/exception-filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ApiResposeInterceptor());
-
+  app.useGlobalFilters(new HttpExceptionFilter());
+  
   const config = new DocumentBuilder()
     .setTitle('Ecommerce API')
     .setDescription('The Ecommerce API description')
