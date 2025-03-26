a5db1b52cf9a58078e65d8ddd3ef8ff2ab67c08e
Gharib84e <ITprofessional63@gmail.com>
2025-03-26 21:48:35 +0100

    gurad had been updated & modified

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -6,9 +6,39 @@ import { Request } from 'express';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService, private configService: ConfigService) {}
-  canActivate(
+ async canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ):Promise<boolean>{
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        {
+          secret: this.configService.get<string>('JWT_SECRET'),
+        },
+      );
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+
     return true;
+  }
+
+  /**
+   * Extracts the token from the Authorization header.
+   * @param request The request from which to extract the token.
+   * @returns The extracted token, or undefined if the header is missing or invalid.
+   */
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    
+    return type === 'Bearer' ? token : undefined;
   }
 }
