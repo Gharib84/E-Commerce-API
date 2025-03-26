2ccbf38bd4b9fedc968c4962c95f7a2f9a972369
Gharib84e <ITprofessional63@gmail.com>
2025-03-26 21:41:47 +0100

    gurad had been created

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -2,8 +2,10 @@ import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from
 import { Observable } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private configService: ConfigService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
