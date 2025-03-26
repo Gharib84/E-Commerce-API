10ec5d16bc69bfc56ad887bc6e6d9d2ccff59358
Gharib84e <ITprofessional63@gmail.com>
2025-03-26 21:56:43 +0100

    gurad had been updated & modified

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,21 +1,20 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private configService: ConfigService) {}
- async canActivate(
+  constructor(private jwtService: JwtService, private configService: ConfigService) { }
+  async canActivate(
     context: ExecutionContext,
-  ):Promise<boolean>{
+  ): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
     if (!token) {
       throw new UnauthorizedException();
     }
-    
+
     try {
       const payload = await this.jwtService.verifyAsync(
         token,
@@ -38,7 +37,7 @@ export class AuthGuard implements CanActivate {
    */
   private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    
+
     return type === 'Bearer' ? token : undefined;
   }
 }
