699a4ab67bfc1ae6d484990455d22d492f86a9c6
Gharib84e <ITprofessional63@gmail.com>
2025-03-26 19:09:23 +0100

    data transfer object -login prcess generated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,20 @@
-import { Controller,Get,Post,Param,Body,Patch,Delete,Query } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Patch, Delete, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 
 @ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('login')
+    login(@Body() user: CreateUserDto) {
+        return this.authService.login(user.username, user.password);
+    }
+
+    @Post('register')
+    register(@Body() user: CreateUserDto) {
+        return this.authService.register(user);
+    }
+}
