1c7cea782d9f31f02c472e88ab15c6053992c625
Gharib84e <ITprofessional63@gmail.com>
2025-03-26 18:40:49 +0100

    auth controller updated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Post,Param,Body,Patch,Delete,Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {}
